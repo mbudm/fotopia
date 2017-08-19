96389a61e65c9e21c5af27d827b6e72b52bf05a1
Steve Roberts <steve@mbudm.com>
2017-08-19 17:04:44 +1000

    What do we have in the 65k files. count by extension

--- index.js
+++ index.js
@@ -12,14 +12,29 @@ const options = {
 // add all these to a Database
 // filter out duplicates, corrupted files
 
-glob("**/*/", options, function (er, files) {
-  console.log('all total:', files.length);
-});
+// glob("**/*", options, function (er, files) {
+//   console.log('all total:', files.length);
+// });
 
 // glob("**/*.{jpg,jpeg,mp4,mov}", options, function (er, files) {
 //   console.log('images total:', files.length);
 // });
 //
-// glob("**/*", {ignore:"**/*.{jpg,jpeg,mp4,mov}",...options}, function (er, files) {
-//   console.log('non images total:', files.length);
-// });
+// What files do we have?
+function getExtension(path){
+  const ext = path.split('.').pop();
+  return ext !== path ? ext : 'no-extension';
+}
+
+glob("**/*", options, function (er, files) {
+  const extensions = files.reduce((knownExtensions, item) => {
+    const ext = getExtension(item);
+    if(knownExtensions[ext]){
+      knownExtensions[ext]++
+    }else{
+      knownExtensions[ext] = 1;
+    }
+    return knownExtensions;
+  }, {});
+  console.log('file types count:', extensions);
+});
