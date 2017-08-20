c6cbc2fb014847e97b5c10b00791d861787a847a
Steve Roberts <steve@mbudm.com>
2017-08-20 10:38:31 +1000

    get all image & movie files

--- index.js
+++ index.js
@@ -12,29 +12,38 @@ const options = {
 // add all these to a Database
 // filter out duplicates, corrupted files
 
-// glob("**/*", options, function (er, files) {
-//   console.log('all total:', files.length);
-// });
-
-// glob("**/*.{jpg,jpeg,mp4,mov}", options, function (er, files) {
-//   console.log('images total:', files.length);
-// });
-//
-// What files do we have?
 function getExtension(path){
   const ext = path.split('.').pop();
   return ext !== path ? ext : 'no-extension';
 }
 
-glob("**/*", options, function (er, files) {
-  const extensions = files.reduce((knownExtensions, item) => {
+function collectExtensions(files){
+  return files.reduce((accumulatedExtensions, item) => {
     const ext = getExtension(item);
-    if(knownExtensions[ext]){
-      knownExtensions[ext]++
+    if(accumulatedExtensions[ext]){
+      accumulatedExtensions[ext]++
     }else{
-      knownExtensions[ext] = 1;
+      accumulatedExtensions[ext] = 1;
     }
-    return knownExtensions;
+    return accumulatedExtensions;
   }, {});
-  console.log('file types count:', extensions);
-});
+}
+
+function getFiles(pattern = "**/*"){
+  return new Promise((resolve, reject) => {
+    glob(pattern, options, function (er, files) {
+      if(er){
+        reject(er);
+      }else{
+        resolve(files)
+      }
+    });
+  });
+}
+//""
+getFiles("**/*.{jpg,jpeg,mp4,mov,avi,png,pmg,gif}")
+  .then((files)=>{
+    const extensions = collectExtensions(files);
+    console.log('file types count:', extensions);
+  })
+  .catch((err) => console.error(err));
